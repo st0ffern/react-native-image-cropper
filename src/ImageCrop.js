6aba9b57c9bb3e158f59e66ea8fd1ff3cf4b4fa9
stoffern <kristoffer.klevenberg@gmail.com>
2016-12-01 08:12:13 +0100

    feat(Algorithm): Changed to the better, movements impoved!

--- src/ImageCrop.js
+++ src/ImageCrop.js
@@ -10,28 +10,83 @@ import {
 import {Surface} from 'gl-react-native'
 const {Image: GLImage} = require("gl-react-image");
 
+const imageDimensionsAfterZoom = (viewport, dimensions, zoom) => {
+  const ImageRatio = dimensions.width/dimensions.height
+  const ViewportRatio = viewport.width/viewport.height
+  if (ImageRatio > ViewportRatio){
+    return {
+      height: Math.floor(viewport.height/zoom),
+      width: Math.floor((viewport.height*ImageRatio)/zoom)
+    }
+  }
+  else
+    return {
+      height: Math.floor((viewport.width/ImageRatio)/zoom),
+      width: Math.floor(viewport.width/zoom)
+    }
+}
+
+const movementFromZoom = (gestureState, viewport, dimensions, offsets, zoom) =>{
+  // X-axis
+  var widthOffset = dimensions.width-viewport.width
+  var pxVsMovX = (1/dimensions.width)
+  var moveX = (gestureState.dx*pxVsMovX) * zoom
+  newPosX = (parseFloat(offsets.x) - parseFloat(moveX))
+
+  // Y-axis
+  var heightOffset = dimensions.height-viewport.height
+  var pxVsMovY = (1/dimensions.height)
+  var moveY = (gestureState.dy*pxVsMovY) * zoom
+  newPosY = (parseFloat(offsets.y) - parseFloat(moveY))
+  return {
+    x: newPosX,
+    y: newPosY,
+  }
+}
+
+
 class ImageCrop extends Component {
   constructor(props) {
     super(props);
     this.state = {
       zoom: 1,
+
       //pan settings
       centerX: 0.5,
       centerY: 0.5,
+
       //Image sizes
       imageHeight: 300,
       imageWidth: 300,
+      imageDimHeight: 0,
+      imageDimWidth: 0,
       currentCapture: '',
     };
   }
   componentWillMount(){
-    var zoom = (100 - this.props.zoom)/100;
-    this.setState({ zoom: zoom })
+    Image.getSize(this.props.image, (width, height) => {
+      //update state
+      this.setState({
+        imageHeight: height,
+        imageWidth: width,
+      });
+    })
 
-    this._panOffsetX= 0;
-    this._panOffsetY= 0;
+    //
+    //get dimensions after crop
+    //
+    this._dimensionAfterZoom = imageDimensionsAfterZoom(
+      {height: this.props.cropHeight, width: this.props.cropWidth},
+      {height: this.state.imageHeight, width: this.state.imageWidth},
+      this.state.zoom 
+    )
+
+    this.setState({
+      imageDimHeight: this._dimensionAfterZoom.height,
+      imageDimWidth: this._dimensionAfterZoom.width
+    })
+
     this._panResponder = PanResponder.create({
-
       onStartShouldSetPanResponder: (evt, gestureState) => true,
       onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
@@ -52,76 +107,84 @@ class ImageCrop extends Component {
       onPanResponderMove: (evt, gestureState) => {
         //We are moving the image
         if (evt.nativeEvent.changedTouches.length <= 1){
-          if (this.props.panToMove){
+          var trackX = (gestureState.dx/this.props.cropWidth)*this.state.zoom;
+          var trackY = (gestureState.dy/this.props.cropHeight)*this.state.zoom;
+          var newPosX = (Number(this.offsetX) - Number(trackX));
+          var newPosY = (Number(this.offsetY) - Number(trackY));
+          if (newPosX> 1) newPosX = Number(1);
+          if (newPosY> 1) newPosY = Number(1);
+          if (newPosX< 0) newPosX = Number(0);
+          if (newPosY< 0) newPosY = Number(0);
 
-            var trackX = (gestureState.dx/this.props.cropWidth)*this.state.zoom;
-              var trackY = (gestureState.dy/this.props.cropHeight)*this.state.zoom;
-            var newPosX = (Number(this.offsetX) - Number(trackX));
-            var newPosY = (Number(this.offsetY) - Number(trackY));
-            if (newPosX> 1) newPosX = Number(1);
-            if (newPosY> 1) newPosY = Number(1);
-            if (newPosX< 0) newPosX = Number(0);
-            if (newPosY< 0) newPosY = Number(0);
-
-            this.setState({centerX: newPosX})
-            this.setState({centerY: newPosY})
-            this._panOffsetX = gestureState.dx*this.state.zoom;
-            this._panOffsetY = gestureState.dy*this.state.zoom;
-          }
+          var movement = movementFromZoom(
+            gestureState, 
+            {width: this.props.cropWidth, height: this.props.cropHeight}, 
+            {width: this.state.imageDimWidth, height: this.state.imageDimHeight}, 
+            {x: this.offsetX, y: this.offsetY}, 
+            this.state.zoom
+          )
+          this.setState({centerX: movement.x})
+          this.setState({centerY: movement.y})
         }else{
         //We are zooming the image
-          if (this.props.pinchToZoom){
-            //Pinch activated
-            if (this.zoomLastDistance == 0){
-              let a = evt.nativeEvent.changedTouches[0].locationX - evt.nativeEvent.changedTouches[1].locationX
-              let b = evt.nativeEvent.changedTouches[0].locationY - evt.nativeEvent.changedTouches[1].locationY
-              let c = Math.sqrt( a*a + b*b );
-              this.zoomLastDistance = c.toFixed(1);
-            }else{
-              let a = evt.nativeEvent.changedTouches[0].locationX - evt.nativeEvent.changedTouches[1].locationX
-              let b = evt.nativeEvent.changedTouches[0].locationY - evt.nativeEvent.changedTouches[1].locationY
-              let c = Math.sqrt( a*a + b*b );
-              this.zoomCurrentDistance = c.toFixed(1);
-              
-              //what is the zoom level
-              var screenDiagonal = Math.sqrt(this.state.imageHeight*this.state.imageHeight + this.state.imageWidth*this.state.imageWidth);
-              var distance = (this.zoomCurrentDistance-this.zoomLastDistance)/400;
-              var zoom = this.state.zoom-distance;
+          if (this.zoomLastDistance == 0){
+            let a = evt.nativeEvent.changedTouches[0].locationX - evt.nativeEvent.changedTouches[1].locationX
+            let b = evt.nativeEvent.changedTouches[0].locationY - evt.nativeEvent.changedTouches[1].locationY
+            let c = Math.sqrt( a*a + b*b );
+            this.zoomLastDistance = c.toFixed(1);
+          }else{
+            let a = evt.nativeEvent.changedTouches[0].locationX - evt.nativeEvent.changedTouches[1].locationX
+            let b = evt.nativeEvent.changedTouches[0].locationY - evt.nativeEvent.changedTouches[1].locationY
+            let c = Math.sqrt( a*a + b*b );
+            this.zoomCurrentDistance = c.toFixed(1);
+            
+            //what is the zoom level
+            var screenDiagonal = Math.sqrt(this.state.imageHeight*this.state.imageHeight + this.state.imageWidth*this.state.imageWidth);
+            var distance = (this.zoomCurrentDistance-this.zoomLastDistance)/400;
+            var zoom = this.state.zoom-distance;
 
-              if (zoom<0.3)zoom=0.3;
-              if (zoom>1)zoom=1;
-              this.setState({
-                zoom: zoom,
-              })
-              //Set last distance..
-              this.zoomLastDistance=this.zoomCurrentDistance;
-            }
+            if (zoom<0)zoom=0.0000001;
+            if (zoom>1)zoom=1;
+            this.setState({
+              zoom: zoom,
+            })
+            //Set last distance..
+            this.zoomLastDistance=this.zoomCurrentDistance;
           }
         }
-      },
-    });
-    Image.getSize(this.props.image, (width, height) => {
-      this.setState({
-        imageHeight: height,
-        imageWidth: width,
-      });
-    });
+      }
+    })
   }
   componentWillReceiveProps(nextProps){
     if (this.props.zoom != nextProps.zoom) {
       var zoom = (100 - nextProps.zoom)/100;
       this.setState({ zoom: zoom });
     }
+
+    //
+    //get dimensions after crop
+    //
+    this._dimensionAfterZoom = imageDimensionsAfterZoom(
+      {height: this.props.cropHeight, width: this.props.cropWidth},
+      {height: this.state.imageHeight, width: this.state.imageWidth},
+      this.state.zoom 
+    )
+
+    this.setState({
+      imageDimHeight: this._dimensionAfterZoom.height,
+      imageDimWidth: this._dimensionAfterZoom.width
+    })
   }
   render() {
     return (
         <View {...this._panResponder.panHandlers}>
           <Surface width={this.props.cropWidth} height={this.props.cropHeight} ref="cropit">
 		        <GLImage
-		          source={{ uri: this.props.image, height: this.state.imageHeight, width: this.state.imageWidth}}
+		          source={{ uri: this.props.image}}
+              imageSize={{height: this.state.imageHeight, width: this.state.imageWidth}}
 		          resizeMode="cover"
 		          zoom={this.state.zoom}
-		          center={[ this.state.centerX, this.state.centerY ]}
+		          center={[this.state.centerX, this.state.centerY]}
 		        />
           </Surface>
         </View>
